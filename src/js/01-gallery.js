13bed53feba8ad20ee5a25e780e64a6d1ffcca0a
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2022-10-26 22:09:11 +0300

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
 // Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const imagesContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMurkup(galleryItems);
+// console.log(cardsMarkup);
+
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+// console.log(galleryItems);
+function createGalleryCardsMurkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" 
+    href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+const lightboxSettings = {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+const lightbox = new SimpleLightbox('.gallery a', lightboxSettings);
 
 console.log(galleryItems);
